5ccbd8debc4b47330db7cab55346cbc27a014327
enoceantours-whales <enoceantours@gmail.com>
2026-05-05 18:10:12 -0700

    Update sightings.js

--- trip-logger/api/sightings.js
+++ trip-logger/api/sightings.js
@@ -4,7 +4,11 @@ const path = require('path');
 module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', 'text/html');
-  const filePath = path.join(process.cwd(), 'trip-logger', 'Public', 'sightings-widget.html');
-  const html = fs.readFileSync(filePath, 'utf8');
-  res.status(200).send(html);
+  try {
+    const filePath = path.join(process.cwd(), 'trip-logger', 'Public', 'sightings-widget.html');
+    const html = fs.readFileSync(filePath, 'utf8');
+    res.status(200).send(html);
+  } catch (err) {
+    res.status(500).send(`Path error: ${err.message} | cwd: ${process.cwd()}`);
+  }
 };
