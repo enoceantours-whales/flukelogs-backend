f5fd1cab26e3b5a2f6a6dd60daf631a99d9ac0ab
enoceantours-whales <enoceantours@gmail.com>
2026-05-05 18:08:40 -0700

    Create sightings.js

---
+++ trip-logger/api/sightings.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+module.exports = async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Content-Type', 'text/html');
+  const filePath = path.join(process.cwd(), 'trip-logger', 'Public', 'sightings-widget.html');
+  const html = fs.readFileSync(filePath, 'utf8');
+  res.status(200).send(html);
+};
